10863bee6bc9c205081ee8a1c4c292b0e8c0dabb
Jessica <diackjessica5@gmail.com>
2025-01-10 13:55:03 -0500

    implement timer

--- src/main/java/org/trafficSimulator/TimingThread.java
+++ src/main/java/org/trafficSimulator/TimingThread.java
@@ -1,4 +1,17 @@
 package org.trafficSimulator;
 
-public class TimingThread {
+public class TimingThread  implements Runnable{
+    private long seconds = 0;
+
+    @Override
+    public void run(){
+        try {
+            while(true) {
+                seconds++;
+                System.out.println("\r" + seconds);
+            }
+        }catch(Exception e){
+            e.getStackTrace();
+        }
+    }
 }
